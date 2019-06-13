b1a0ef3cdd25cdc8a2dce33b60b06f042debe70c
Julien Fouquet <jfouquet@INFOMIL-P890.local>
2019-06-13 16:45:07 +0200

    second

---
+++ server.js
@@ -0,0 +1,39 @@
+// Constants
+const express = require('express');
+const app = new express();
+const port = process.env.PORT || 4800;
+const path = require("path");
+const bodyParser = require('body-parser');
+
+// Variable
+var savedHistory = null;
+
+// To load static files
+express.static('/public');
+app.use(express.static('public'));
+// Tell express to use the body-parser middleware and to not parse extended bodies
+app.use( bodyParser.json() ); 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Start on index.html file
+app.get('/', function(request, response){
+  response.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/order', function(request, response, next) {
+  const body = request.body;
+  let date = new Date();
+  console.log('* ' + date.getHours() + 'h' + ('0' + date.getMinutes()).slice(-2) + ' - ' + body.commande);
+
+  response.send();
+});
+
+// Listen to the port
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log('--------------------------------------------');
+  console.log('Start server');
+  console.log('--------------------------------------------');
+})
